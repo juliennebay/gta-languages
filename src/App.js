6d1b1d6ceab02c6da3e25a15abc1d831e2d91936
Julie <julienne.bay@gmail.com>
2020-07-16 17:59:09 -0400

    display data by showing stacked bar graphs

--- src/App.js
+++ src/App.js
@@ -13,78 +13,84 @@ const DATA_OBJECT = {
   Oakville: OakvilleData
 };
 
-function ChartTest() {
-  const data = React.useMemo(() => [
-    {
-      label: "Series 1",
-      data: [{ x: 1, y: 10 }, { x: 2, y: 20 }, { x: 3, y: 30 }]
-    }
-  ]);
+//what ChartTest data (props.data) must look like:
+
+//[{
+//label: "City name"
+//data: [{x: label (mother tongue, etc.), y: total # of speakers}]
+//},]
+
+function ChartTest(props) {
   const axes = React.useMemo(
     () => [
-      { primary: true, type: "linear", position: "bottom" },
-      { type: "linear", position: "right" }
+      { primary: true, type: "ordinal", position: "bottom" },
+      { type: "linear", position: "left", stacked: true }
     ],
     []
   );
+
   const series = React.useMemo(
     () => ({
       type: "bar"
     }),
     []
   );
+
   return (
     <div
       style={{
-        width: "400px",
-        height: "300px"
+        width: "900px",
+        height: "900px"
       }}
     >
-      <Chart data={data} axes={axes} />
+      <Chart data={props.data} axes={axes} series={series} tooltip />
     </div>
   );
 }
-
+//keep in mind: data = an array of objects
+//the output of the first filter (after tbody) is an array of length x (ex. 3 different categories under Korean - Mother tongue, language spoken at home, etc.)
 function CityData({ data, language }) {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Matched string</th>
-          <th>Label</th>
-          <th>Total # of speakers</th>
-          <th>Total # of single responses</th>
-          <th>% of the total single responses</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data
-          .filter(d => d.TEXT_NAME_NOM.toLowerCase().includes(language))
-          .map(d => ({
-            matchedStr: d.TEXT_NAME_NOM,
-            label: data.find(obj => obj.HIER_ID === d.HIER_ID.slice(0, 5))
-              .TEXT_NAME_NOM,
-            total: d.T_DATA_DONNEE,
-            singleResponses: data.find(
-              obj => obj.HIER_ID === d.HIER_ID.slice(0, 7)
-            ).T_DATA_DONNEE,
-            percentage:
-              d.T_DATA_DONNEE /
-              data.find(obj => obj.HIER_ID === d.HIER_ID.slice(0, 7))
-                .T_DATA_DONNEE
-          }))
-          .sort((a, b) => a.matchedStr.localeCompare(b.matchedStr))
-          .map((d, i) => (
-            <tr key={i}>
-              <td>{d.matchedStr}</td>
-              <td>{d.label}</td>
-              <td>{d.total}</td>
-              <td>{d.singleResponses}</td>
-              <td>{(d.percentage * 100).toFixed(2)}%</td>
-            </tr>
-          ))}
-      </tbody>
-    </table>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Matched string</th>
+            <th>Label</th>
+            <th>Total # of speakers</th>
+            <th>Total # of single responses</th>
+            <th>% of the total single responses</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data
+            .filter(d => d.TEXT_NAME_NOM.toLowerCase().includes(language))
+            .map(d => ({
+              matchedStr: d.TEXT_NAME_NOM,
+              label: data.find(obj => obj.HIER_ID === d.HIER_ID.slice(0, 5))
+                .TEXT_NAME_NOM,
+              total: d.T_DATA_DONNEE,
+              singleResponses: data.find(
+                obj => obj.HIER_ID === d.HIER_ID.slice(0, 7)
+              ).T_DATA_DONNEE,
+              percentage:
+                d.T_DATA_DONNEE /
+                data.find(obj => obj.HIER_ID === d.HIER_ID.slice(0, 7))
+                  .T_DATA_DONNEE
+            }))
+            .sort((a, b) => a.matchedStr.localeCompare(b.matchedStr))
+            .map((d, i) => (
+              <tr key={i}>
+                <td>{d.matchedStr}</td>
+                <td>{d.label}</td>
+                <td>{d.total}</td>
+                <td>{d.singleResponses}</td>
+                <td>{(d.percentage * 100).toFixed(2)}%</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
@@ -93,12 +99,30 @@ function App() {
 
   return (
     <div className="App">
-      <ChartTest />
       <h1>The # of speakers of a language in the GTA</h1>
       <input
         onChange={e => setLanguage(e.target.value.toLowerCase())}
         placeholder="search language"
       ></input>
+      {selectedLanguage.length ? (
+        <ChartTest
+          data={Object.keys(DATA_OBJECT).map(cityName => {
+            return {
+              label: cityName,
+              data: DATA_OBJECT[cityName]
+                .filter(d =>
+                  d.TEXT_NAME_NOM.toLowerCase().includes(selectedLanguage)
+                )
+                .map(d => ({
+                  x: DATA_OBJECT[cityName].find(
+                    obj => obj.HIER_ID === d.HIER_ID.slice(0, 5)
+                  ).TEXT_NAME_NOM,
+                  y: d.T_DATA_DONNEE
+                }))
+            };
+          })}
+        />
+      ) : null}
       {selectedLanguage.length
         ? Object.keys(DATA_OBJECT).map(cityName => (
             <div>
